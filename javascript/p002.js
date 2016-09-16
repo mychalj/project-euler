ff1cbde9a0b6ef85302db4cf27ee4890d57c93be
Mychal <mychalqj@gmail.com>
2016-09-16 16:45:25 -0400

    Program now calculates correct answer

--- javascript/p002.js
+++ javascript/p002.js
@@ -8,12 +8,23 @@
 var fibNums = [1, 2];
 var iOne = 0;
 var iTwo = 1;
+var i = 0;
+var evenNums = [];
 
-while (fibNums[iTwo] < 89) {
+while (fibNums[iTwo] < 4000000) {
   num = fibNums[iOne] + fibNums[iTwo];
   fibNums.push(num);
   iOne += 1;
   iTwo += 1;
 }
 
-console.log(fibNums);
+while (fibNums[i] < 4000000) {
+  if (fibNums[i] % 2 === 0) {
+    evenNums.push(fibNums[i]);
+  }
+  i += 1;
+}
+
+var total = evenNums.reduce(function(sum, num){return sum + num;});
+
+console.log(total);
