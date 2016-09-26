9434ba476bee5a59ae13562b81cae677a9a78f13
Mychal <mychalqj@gmail.com>
2016-09-26 13:21:07 -0400

    Problem completed

--- ruby/p003.rb
+++ ruby/p003.rb
@@ -4,3 +4,20 @@
  *
  * https://github.com/mychalj/project-euler
 =end
+
+#This is the number that the function will find the largest prime factor(lpm) of.
+num = 600851475143
+#This variable is used to iterate through the loop to find the lpm.
+x = 2
+
+#This while loop keeps going until the desired lpm is stored in x.
+while x < num do
+  if num % x == 0
+    num /= x
+  else
+    x += 1
+  end
+end
+
+#This outputs the lpm stored in x.
+puts x
