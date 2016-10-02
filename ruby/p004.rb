82361e311a4f086dbb32014a53356ada0b4081bd
Mychal <mychalqj@gmail.com>
2016-10-02 15:41:03 -0400

    Completed p004

--- ruby/p004.rb
+++ ruby/p004.rb
@@ -5,3 +5,24 @@
  * https://github.com/mychalj/project-euler
 =end
 
+def isPalindrome(num)
+  if num == num.to_s.split("").reverse().join("").to_i
+    return true
+  else
+    return false
+  end
+end
+
+product = 0
+max = 0
+
+(100..999).each do |i|
+  (100..999).each do |j|
+    product = i * j
+    if isPalindrome(product)
+      max = product if max < product
+    end
+  end
+end
+
+puts "The largest palindrome made from the product of two 3-digit numbers is #{max}."
