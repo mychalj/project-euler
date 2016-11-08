ff1adc9fdebc8cc6ec19038130fecb60cb1d725e
Mychal <mychalqj@gmail.com>
2016-11-08 18:47:58 -0500

    Completed problem

--- javascript/p005.js
+++ javascript/p005.js
@@ -6,48 +6,48 @@
  */
 
 // Determines if a number is prime
-function isPrime(num) {
+function isPrime (num) {
   for (var i = 2; i < num; i++) {
-    if (num % i === 0) { return false; }
+    if (num % i === 0) { return false }
   }
-  return true;
+  return true
 }
 
 // Creates an array that contains the prime number to start with in
 // the smallestMultiple function and the largest power of each of
 // the prime numbers less than and including the start number.
-function largestPower(num) {
-  var powers = [];
-  var index = 0;
-  var start = 0;
+function largestPower (num) {
+  var powers = []
+  var index = 0
+  var start = 0
 
   for (var i = 2; i <= num; i++) {
     for (var j = 2; j <= num; j++) {
       if (isPrime(i) && Math.pow(i, j) <= num) {
-        powers[index] = Math.pow(i, j);
-        start = i;
+        powers[index] = Math.pow(i, j)
+        start = i
       }
     }
     index++
   }
-  powers.unshift(start);
-  return powers;
+  powers.unshift(start)
+  return powers
 }
 
 // Determines the smallest number that is evenly divisible by all
 // numbers from 1 to the given positive number.
-function smallestMultiple(limit) {
-  var powersArr = (largestPower(limit)).slice(1);
-  var start = (largestPower(limit)).unshift();
-  var answer = powersArr.reduce(function(a, b) { return a * b; });
+function smallestMultiple (limit) {
+  var powersArr = (largestPower(limit)).slice(1)
+  var start = (largestPower(limit)).unshift()
+  var answer = powersArr.reduce(function (a, b) { return a * b })
 
   for (var i = start + 1; i <= limit; i++) {
     if (isPrime(i)) {
-      answer *= i;
+      answer *= i
     }
   }
-  return answer;
+  return answer
 }
 
-// Displays the desired answered
-console.log(smallestMultiple(20));
+// Displays the desired answer
+console.log(smallestMultiple(20))
