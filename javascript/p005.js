d138466fd6d443fce8b4e9e29332cfeedc54981b
Mychal <mychalqj@gmail.com>
2016-11-08 17:30:07 -0500

    Completed problem

--- javascript/p005.js
+++ javascript/p005.js
@@ -11,3 +11,36 @@ function isPrime(num) {
   }
   return true;
 }
+
+function largestPower(num) {
+  var powers = [];
+  var index = 0;
+  var start = 0;
+
+  for (var i = 2; i <= num; i++) {
+    for (var j = 2; j <= num; j++) {
+      if (isPrime(i) && Math.pow(i, j) <= num) {
+        powers[index] = Math.pow(i, j);
+        start = i;
+      }
+    }
+    index++
+  }
+  powers.unshift(start);
+  return powers;
+}
+
+function smallestMultiple(limit) {
+  var powersArr = (largestPower(limit)).slice(1);
+  var start = (largestPower(limit)).unshift();
+  var answer = powersArr.reduce(function(a, b) { return a * b; });
+
+  for (var i = start + 1; i <= limit; i++) {
+    if (isPrime(i)) {
+      answer *= i;
+    }
+  }
+  return answer;
+}
+
+console.log(smallestMultiple(20));
