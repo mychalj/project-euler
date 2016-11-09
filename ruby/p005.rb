50c6764449248c184639764141e4402251821a0c
Mychal <mychalqj@gmail.com>
2016-11-09 11:23:25 -0500

    Added largest_power method

--- ruby/p005.rb
+++ ruby/p005.rb
@@ -4,6 +4,22 @@
 
 # Determines if a number is prime
 def prime?(num)
-  Math.sqrt(num).to_i.downto(2).each { return false if (num % i).zero? }
+  Math.sqrt(num).to_i.downto(2).each { |i| return false if (num % i).zero? }
   true
+end
+
+def largest_power(num)
+  powers = []
+  arr_index, start = 0
+
+  (2..num).each do |i|
+    (2..num).each do |j|
+      if prime?(i) && (i**j) <= num
+        powers[arr_index] = i**j
+        start = i
+      end
+    end
+    arr_index += 1
+  end
+  powers.unshift(start)
 end
