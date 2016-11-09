8d2241c9a71c1f63b2adc3964fffb3dc0a38c43d
Mychal <mychalqj@gmail.com>
2016-11-09 10:38:58 -0500

    Added prime? method

--- ruby/p005.rb
+++ ruby/p005.rb
@@ -1,6 +1,11 @@
-=begin
- * Ruby Solution to Project Euler problem 5
- * by Mychal Johnson
- *
- * https://github.com/mychalj/project-euler
-=end
+# Ruby Solution to Project Euler problem 5
+# by Mychal Johnson
+# https://github.com/mychalj/project-euler
+
+# Determines if a number is prime
+def prime?(num)
+  (2..num).each do |i|
+    return false if (num % i).zero?
+    return true
+  end
+end
