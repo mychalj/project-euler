dfee2153cee6fdea652920bbae8ba728f5e82cd4
Mychal <mychalqj@gmail.com>
2016-11-09 11:20:18 -0500

    Changed prime? method

--- ruby/p005.rb
+++ ruby/p005.rb
@@ -4,8 +4,6 @@
 
 # Determines if a number is prime
 def prime?(num)
-  (2..num).each do |i|
-    return false if (num % i).zero?
-    return true
-  end
+  Math.sqrt(num).to_i.downto(2).each { return false if (num % i).zero? }
+  true
 end
