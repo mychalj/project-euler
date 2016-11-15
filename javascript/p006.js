177052e52a9d30b7970d65e0c19825fd44565de2
Mychal <mychalqj@gmail.com>
2016-11-15 17:05:10 -0500

    Added prime method

--- javascript/p006.js
+++ javascript/p006.js
@@ -4,3 +4,11 @@
  *
  * https://github.com/mychalj/project-euler
  */
+
+// Determines if a number is prime
+function isPrime (num) {
+  for (var i = 2; i < num; i++) {
+    if (num % i === 0) { return false }
+  }
+  return true
+}
