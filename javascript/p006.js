f7e77e49af912fc09827202175941dcef1006337
Mychal <mychalqj@gmail.com>
2016-11-15 17:51:20 -0500

    Completed problem

--- javascript/p006.js
+++ javascript/p006.js
@@ -5,10 +5,23 @@
  * https://github.com/mychalj/project-euler
  */
 
-var sum_of_squares = 0
-var square_of_sum = 0
+// Calculates the difference between the sum of squares of the first
+// (num variable) natural numbers and the square of the sum
+function sum_square_diff(num) {
+  var sum_of_squares = 0
+  var square_of_sum = 0
 
-for (var i = 1; i <= 100; i++) {
-  var squared = Math.pow(i, 2)
-  sum_of_squares += squared
+  for (var i = 1; i <= num; i++) {
+    var squared = Math.pow(i, 2)
+    sum_of_squares += squared
+    square_of_sum += i
+  }
+
+  square_of_sum = Math.pow(square_of_sum, 2)
+
+  var difference = square_of_sum - sum_of_squares
+
+  return difference
 }
+
+console.log(sum_square_diff(100))
--- ruby/p006.rb
+++ ruby/p006.rb
@@ -2,10 +2,23 @@
 # by Mychal Johnson
 # https://github.com/mychalj/project-euler
 
-sum_of_squares = 0
-square_of_sum = 0
+# Calculates the difference between the sum of squares of the first
+# (num variable) natural numbers and the square of the sum
+def sum_square_diff(num)
+  sum_of_squares = 0
+  square_of_sum = 0
 
-for i in 1..100
-  squared = i**2
-  sum_of_squares += squared
+  (1..num).each do |i|
+    squared = i**2
+    sum_of_squares += squared
+    square_of_sum += i
+  end
+
+  square_of_sum **= 2
+
+  difference = square_of_sum - sum_of_squares
+
+  puts difference
 end
+
+sum_square_diff(100)
