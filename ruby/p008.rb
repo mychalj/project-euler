948d08b924fa92e3cc79bd9bbefd3cda773470c9
Mychal <mychalqj@gmail.com>
2016-11-24 11:39:09 -0500

    Added needed digits to an array

--- ruby/p008.rb
+++ ruby/p008.rb
@@ -1,11 +1,8 @@
-/*
- * Javascript Solution to Project Euler problem 8
- * by Mychal Johnson
- *
- * https://github.com/mychalj/project-euler
- */
+ # Ruby Solution to Project Euler problem 8
+ # by Mychal Johnson
+ # https://github.com/mychalj/project-euler
 
-var thousand_digits =
+ thousand_digits =
 [[73167176531330624919225119674426574742355349194934],
 [96983520312774506326239578318016984801869478851843],
 [85861560789112949495459501737958331952853208805511],
