d3bdd908ff714ed3a5b851d5d933102125f205ed
Mychal <mychalqj@gmail.com>
2016-12-02 11:03:53 -0500

    Created p009 file

---
+++ javascript/p009.js
@@ -0,0 +1,6 @@
+/*
+ * Javascript Solution to Project Euler problem 9
+ * by Mychal Johnson
+ *
+ * https://github.com/mychalj/project-euler
+ */
---
+++ ruby/p009.rb
@@ -0,0 +1,3 @@
+# Ruby Solution to Project Euler problem 9
+# by Mychal Johnson
+# https://github.com/mychalj/project-euler
