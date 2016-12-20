ae5b58cb03067e42721c87fc37195586f9c9ca49
Mychal <mychalqj@gmail.com>
2016-12-20 18:53:33 -0500

    Created p010 file

---
+++ javascript/p010.js
@@ -0,0 +1,10 @@
+/*
+ * Javascript Solution to Project Euler problem 10
+ * by Mychal Johnson
+ *
+ * https://github.com/mychalj/project-euler
+ */
+
+function sum_of_primes(limit){}
+
+console.log(sum_of_primes())
---
+++ ruby/p010.rb
@@ -0,0 +1,3 @@
+# Ruby Solution to Project Euler problem 10
+# by Mychal Johnson
+# https://github.com/mychalj/project-euler
