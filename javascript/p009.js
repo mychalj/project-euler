b05b50b8ba8c7c67c9a0c1bbf9746817ff757fe3
Mychal <mychalqj@gmail.com>
2016-12-20 13:42:27 -0500

    Added variables

--- javascript/p009.js
+++ javascript/p009.js
@@ -5,5 +5,8 @@
  * https://github.com/mychalj/project-euler
  */
 
-function pythagorean_triplet(prod) {
+function pythagorean_triplet(product) {
+  var a = Math.sqrt(m) - Math.sqrt(n)
+  var b = Math.sqrt(m * n)
+  var c = Math.sqrt(m) + Math.sqrt(n)
 }
--- ruby/p009.rb
+++ ruby/p009.rb
@@ -2,5 +2,8 @@
 # by Mychal Johnson
 # https://github.com/mychalj/project-euler
 
-def pythagorean_triplet(prod)
+def pythagorean_triplet(product)
+  a = Math.sqrt(m) - Math.sqrt(n)
+  b = Math.sqrt(m * n)
+  c = Math.sqrt(m) + Math.sqrt(n)
 end
