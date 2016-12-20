909602e78a6d9b3f1e9dbb22809cbc0cd311850b
Mychal <mychalqj@gmail.com>
2016-12-20 18:43:11 -0500

    Completed problem

--- ruby/p009.rb
+++ ruby/p009.rb
@@ -2,8 +2,14 @@
 # by Mychal Johnson
 # https://github.com/mychalj/project-euler
 
-def pythagorean_triplet(product)
-  a = (m ** 2) - (n ** 2)
-  b = (m * n) ** 2
-  c = (m ** 2) + (n ** 2)
+def pythagorean_triplet(limit)
+  (1..limit).each do |a|
+    ((a + 1)..limit).each do |b|
+      c = limit - a - b
+
+      puts a * b * c if a**2 + b**2 == c**2
+    end
+  end
 end
+
+pythagorean_triplet(1000)
