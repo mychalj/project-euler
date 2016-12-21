0a010a89f959a9dd4e1843814f2e36f11f72a9c2
Mychal <mychalqj@gmail.com>
2016-12-20 19:20:03 -0500

    sum_of_prime functional

--- javascript/p010.js
+++ javascript/p010.js
@@ -5,6 +5,7 @@
  * https://github.com/mychalj/project-euler
  */
 
+// Determines if a number is prime
 function is_prime (num) {
   for (var i = 2; i < num; i++) {
     if (num % i === 0) { return false }
@@ -12,7 +13,16 @@ function is_prime (num) {
   return true
 }
 
+// Finds the sum of all the primes below 'limit'
 function sum_of_primes(limit){
+  var answer = 0
+
+  for (var i = 2; i < limit; i++) {
+    if (is_prime(i)) {
+      answer = answer + i
+    }
+  }
+  return answer
 }
 
-console.log(sum_of_primes())
+console.log(sum_of_primes(2000000))
--- ruby/p010.rb
+++ ruby/p010.rb
@@ -2,12 +2,20 @@
 # by Mychal Johnson
 # https://github.com/mychalj/project-euler
 
+# Determines if a number is prime
 def prime?(num)
   Math.sqrt(num).to_i.downto(2).each { |i| return false if (num % i).zero? }
   true
 end
 
+# Finds the sum of all the primes below 'limit'
 def sum_of_primes(limit)
+  answer = 0
+
+  (2...limit).each do |i|
+    answer = answer + i if prime?(i)
+  end
+  puts answer
 end
 
-sum_of_primes()
+sum_of_primes(2000000)
